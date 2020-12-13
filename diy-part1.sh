d4a9d35e612481ccd82ac5726bc715735781ec2f
liuybgh <58780029+liuybgh@users.noreply.github.com>
2020-12-13 12:48:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/project-openwrt/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
