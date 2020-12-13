14b56daa2c4f3caf5cc78485537aad3882b82972
liuybgh <58780029+liuybgh@users.noreply.github.com>
2020-12-13 12:51:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git packages https://github.com/coolsnowwolf/packages' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
+sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
+sed -i '$a src-git freifunk https://github.com/freifunk/openwrt-packages.git' feeds.conf.default
 
+# Add Apps
 git clone https://github.com/project-openwrt/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
